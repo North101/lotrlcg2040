2c80192ebf49f6c46799d9c561d9d0dfc9a87852
Alexander Wilde <alexander.wilde87@gmail.com>
2022-11-21 18:45:52 +0000

    add assets_dir

--- lotrlcg2040/__init__.py
+++ lotrlcg2040/__init__.py
@@ -2,6 +2,7 @@ from lotrlcg2040.data import eye_icon
 from lotrlcg2040.widgets.app import MyApp
 
 root_dir = '/'.join(__file__.rsplit('/')[:-1])
+assets_dir = f'{root_dir}/assets'
 
 def start():
   eye_icon.load()
--- lotrlcg2040/data.py
+++ lotrlcg2040/data.py
@@ -2,4 +2,4 @@ from badger_ui.util import Image
 
 import lotrlcg2040
 
-eye_icon = Image(f'{lotrlcg2040.root_dir}/assets/eye.bin', 64, 64)
+eye_icon = Image(f'{lotrlcg2040.assets_dir}/eye.bin', 64, 64)
